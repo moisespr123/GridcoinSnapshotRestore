9f91698d32856c999e9ef3a47b4c468be62b941f
moisesmcardona <moises@moises-studios.com>
2017-04-01 19:44:52 -0400

    Minor layout change

--- GridcoinSnapshotRestore/Form1.Designer.vb
+++ GridcoinSnapshotRestore/Form1.Designer.vb
@@ -102,7 +102,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(294, 176)
+        Me.ClientSize = New System.Drawing.Size(283, 176)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
